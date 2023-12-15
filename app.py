173d3b1afbb51d729f9bcd6c7c87bddcc92926aa
mosesdaudu001 <mosesdaudu001@gmail.com>
2023-12-15 12:41:45 +0100

    added gemini ai pro

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template, request, jsonify
 import vertexai
 from vertexai.language_models import CodeChatModel
+from vertexai.preview.generative_models import GenerativeModel, Part
+
 import os
 from dotenv import load_dotenv
 
@@ -14,14 +16,21 @@ CODE_CHAT_MODEL = os.getenv('CODE_CHAT_MODEL')
 app = Flask(__name__)
 
 vertexai.init(project=PROJECT_ID, location=LOCATION)
-chat_model = CodeChatModel.from_pretrained(CODE_CHAT_MODEL)
-parameters = {
-    "candidate_count": 1,
-    "max_output_tokens": 1024,
-    "temperature": 0.2
-}
-chat = chat_model.start_chat()
+# chat_model = CodeChatModel.from_pretrained(CODE_CHAT_MODEL)
+# parameters = {
+#     "candidate_count": 1,
+#     "max_output_tokens": 1024,
+#     "temperature": 0.2
+# }
+# chat = chat_model.start_chat()
 
+config = {
+    "max_output_tokens": 2048,
+    "temperature": 0.9,
+    "top_p": 1
+}
+model = GenerativeModel(CODE_CHAT_MODEL)
+chat = model.start_chat()
 
 @app.route("/")
 def home():
@@ -30,7 +39,8 @@ def home():
 @app.route("/get")
 def get_bot_response():    
     userText = request.args.get('msg')
-    response = chat.send_message(userText, **parameters)
+    # response = chat.send_message(userText, **parameters) # for the bison model
+    response = chat.send_message(userText, generation_config=config) # for the gemini AI model
     # print(f"Response from Model: {response.text}")
 
     return response.text
